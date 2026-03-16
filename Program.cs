ec32b5d017daaff549311343d9b72605c98dc960
emanu <emanu@EMANUEL_RAMREZ>
2026-03-15 22:46:03 -0600

    Implementación de Do While en el proyecto!

--- Program.cs
+++ Program.cs
@@ -2,11 +2,27 @@
 
 
 
-Console.WriteLine("===== SISTEMA DE EVALUACION DE CONTENIDO ======");
-Console.WriteLine("1. Evaluar nuevo contenido");
-Console.WriteLine("2. Mostrar reglas del sistema");
-Console.WriteLine("3. Mostrar estadisticas ");
-Console.WriteLine("4. Reiniciar estadisticas");
-Console.WriteLine("5. Salir");
+int opcion;
 
-Console.WriteLine("Presione uno de los 5 números para seleccionar una opción!...");
+do
+{
+    Console.WriteLine("===== SISTEMA DE EVALUACION DE CONTENIDO ======");
+    Console.WriteLine("1. Evaluar nuevo contenido");
+    Console.WriteLine("2. Mostrar reglas del sistema");
+    Console.WriteLine("3. Mostrar estadisticas ");
+    Console.WriteLine("4. Reiniciar estadisticas");
+    Console.WriteLine("5. Salir");
+
+    Console.WriteLine("Presione uno de los 5 números para seleccionar una opción!...");
+
+    opcion = int.Parse(Console.ReadLine());
+
+  
+   
+
+
+
+
+
+
+} while (opcion != 5);
