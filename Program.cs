6e9d36bd22a2bb1a5cbdbf995d83307fee3f61eb
emanu <emanu@EMANUEL_RAMREZ>
2026-03-16 08:33:29 -0600

    Switch implementado !!

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿// SIMULADOR DE DECISIONES PARA PLATAFORMA DE STREAMING
 
-
-
 int opcion;
 
 do
@@ -17,12 +15,53 @@ do
 
     opcion = int.Parse(Console.ReadLine());
 
-  
-   
+    switch (opcion)
+    {
+
+        case 1:
+            {
 
 
 
+                break;
+            }
+
+        case 2:
+            {
+
+                break;
+            }
+
+        case 3:
+            {
+
+                break;
+            }
+
+        case 4:
+            {
+                break;
+            }
+
+        case 5:
+            {
+
+            }
+
+            break;
 
 
+        default{
+
+
+                break;
+            }
+
+
+    }
+
+
+
+       
 
 } while (opcion != 5);
