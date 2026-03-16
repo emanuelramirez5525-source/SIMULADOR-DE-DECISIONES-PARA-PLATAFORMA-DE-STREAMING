55383bf0d1fde5b4075c60f224bed3e379225bba
emanu <emanu@EMANUEL_RAMREZ>
2026-03-15 22:39:03 -0600

    Menu de Sistemas!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 ﻿// SIMULADOR DE DECISIONES PARA PLATAFORMA DE STREAMING
+
+
+
+Console.WriteLine("===== SISTEMA DE EVALUACION DE CONTENIDO ======");
+Console.WriteLine("1. Evaluar nuevo contenido");
+Console.WriteLine("2. Mostrar reglas del sistema");
+Console.WriteLine("3. Mostrar estadisticas ");
+Console.WriteLine("4. Reiniciar estadisticas");
+Console.WriteLine("5. Salir");
+
+Console.WriteLine("Presione uno de los 5 números para seleccionar una opción!...");
