7823a5f4edad6bdeb2bd50399d5569ed21360458
emanu <emanu@EMANUEL_RAMREZ>
2026-03-19 23:00:52 -0600

    Arregle Switch (error minimo), agregue la primera opcion con funciones: Evaluar contenido,validación tecnica y impacto if else anidado y encadenado implementado!

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿// SIMULADOR DE DECISIONES PARA PLATAFORMA DE STREAMING
 
+int totalEvaluados = 0;
+int publicados = 0;
+int rechazados = 0;
+int enRevision = 0;
+
+int impactoAlto = 0;
+int impactoMedio = 0;
+int impactoBajo = 0;
+
 int opcion;
 
 do
@@ -11,7 +20,7 @@ do
     Console.WriteLine("4. Reiniciar estadisticas");
     Console.WriteLine("5. Salir");
 
-    Console.WriteLine("Presione uno de los 5 números para seleccionar una opción!...");
+    Console.Write("Presione uno de los 5 números para seleccionar una opción:");
 
     opcion = int.Parse(Console.ReadLine());
 
@@ -20,14 +29,13 @@ do
 
         case 1:
             {
-
-
-
+                EvaluarContenido();
                 break;
             }
 
         case 2:
             {
+
 
                 break;
             }
@@ -45,13 +53,14 @@ do
 
         case 5:
             {
-
+                Console.WriteLine("Resumen Final de la decisión");
             }
 
             break;
 
 
-        default{
+        default:
+            {
 
 
                 break;
@@ -65,3 +74,133 @@ do
        
 
 } while (opcion != 5);
+
+void EvaluarContenido()
+{
+    Console.WriteLine("#======= Evaluar Tipo de Contenido ==========#");
+
+    Console.WriteLine("Ingrese el Tipo de contenido: (película, serie, documental, evento en vivo): ");
+    string tipo = Console.ReadLine().ToLower();
+
+
+    Console.Write("Ingrese la Duración en minutos: ");
+    int duracion = int.Parse(Console.ReadLine());
+
+    Console.Write("Ingrese la Clasificación (Todo público, +13, +18): ");
+    string clasificacion = Console.ReadLine().ToLower();
+ 
+    Console.Write("Ingrese la Hora Programada (0-23):");
+    int hora = int.Parse(Console.ReadLine());
+
+    Console.Write("Ingrese el Nivel de Producción: (bajo, medio, alto)");
+    string produccion = Console.ReadLine().ToLower();
+
+    (bool valido, string mensaje) = ValidacionTecnica(tipo, duracion, clasificacion, hora, produccion);
+
+    string impacto = "N/A";
+    string decision= "";
+
+    if (!valido)
+    {
+        Console.WriteLine(mensaje);
+        decision = "Rechazar";
+        rechazados++;
+    }
+
+    else
+    {
+        impacto = ClasificacionImpacto(produccion, duracion, hora);
+
+        if (impacto == "Alto")
+        {
+            decision = "Enviar revisión";
+            enRevision++;
+        }
+        else
+        {
+            decision = "Publicar";
+            publicados++;
+        }
+
+    }
+
+    totalEvaluados++;
+    Console.WriteLine("==== RESULTADOS =====");
+    Console.WriteLine("Impacto: "+ impacto);
+    Console.WriteLine("Decisión: "+ decision);
+}
+
+
+
+(bool,string) ValidacionTecnica(string tipo, int duracion, string clasificacion, int hora, string produccion)
+{
+    if (tipo == "pelicula" && (duracion < 60 || duracion > 180))
+        return (false, "ERROR!!! Duracion invalida para la película");
+
+
+    if (tipo == "serie" && (duracion < 20 || duracion > 90))
+        return (false, "ERROR!!! Duracion invalida para la serie");
+
+
+    if (tipo == "documental" && (duracion < 30 || duracion > 120))
+        return (false, "ERROR!!! Duracion invalida para el documental");
+
+
+    if (tipo == "evento" && (duracion < 30 || duracion > 240))
+        return (false, "ERROR!!! Duracion invalida para el evento");
+
+
+    if (clasificacion == "+13" && (hora < 6 || hora > 22))
+        return (false, "ERROR!!! Horario no permitido para  +13");
+
+
+    if (clasificacion == "+18" && !(hora >= 22|| hora <=5))
+        return (false, "ERROR!!! Horario no permitido para +18");
+
+
+    if (produccion == "bajo" &&  clasificacion == "+18")
+        return (false, "ERROR!!! Producción no permitida para +18");
+
+    return (true, "Contenido valido");
+}
+
+string ClasificacionImpacto(string produccion, int duracion, int hora)
+{
+    string impacto = "Bajo";
+
+    if (produccion == "alto" || duracion > 120 || (hora >=20 && hora <=23))
+    {
+        impacto = "Alto";
+        impactoAlto++;
+    }
+
+    else if (produccion == "medio" || (duracion >= 60 && duracion <= 120))
+    {
+        impacto = "Medio";
+        impactoMedio++;
+    }
+
+    else
+    {
+        impacto = "Bajo";
+        impactoBajo++;  
+
+    }
+    return impacto;
+}
+
+void MostrarReglas()
+    {
+    
+
+    }
+
+void MostrarEstadisticas()
+{
+
+}
+
+void ReiniciarEstadisticas()
+{
+
+}
